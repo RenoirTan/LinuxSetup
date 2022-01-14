d5f636491a97eea6039864af82337b4ba8b2b54a
RenoirTan <renoirtan2005@gmail.com>
2022-01-14 11:17:00 +0800

    main: add functions for backing up and restoring config files/folders

--- utils.sh
+++ utils.sh
@@ -6,6 +6,14 @@ lxspr_echof() {
 	echo "$(lxspr_path $1)" "$1"
 }
 
+lxspr_backup() {
+	cp -r "$1" "$1.old"
+}
+
+lxspr_restore() {
+	mv "$1.old" "$1"
+}
+
 lxspr_cp() {
 	cp "$(lxspr_path $1)" "$1"
 }
