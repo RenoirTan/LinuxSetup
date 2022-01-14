da4971475285ac7fc0231be605a966643a2fc591
RenoirTan <renoirtan2005@gmail.com>
2022-01-14 11:31:23 +0800

    main: add sddm setup scripts

---
+++ agnostic/restore-sddm.sh
@@ -0,0 +1,7 @@
+source ../utils.sh
+
+lxspr_restore /etc/sddm.conf
+lxspr_restore /etc/sddm.conf.d
+
+echo "Restart SDDM for the changes to take effect."
+
---
+++ agnostic/setup-sddm.sh
@@ -0,0 +1,5 @@
+source ../utils.sh
+
+lxspr_replace /etc/sddm.conf
+lxspr_replace /etc/sddm.conf.d
+
