d236176a20f7cfac09582ee6e3a79067537c9285
RenoirTan <renoirtan2005@gmail.com>
2022-01-14 11:17:52 +0800

    main: add my systemd configs

---
+++ agnostic/restore-systemd.sh
@@ -0,0 +1,4 @@
+source ../utils.sh
+
+mv /etc/systemd/logind.conf.lxspold /etc/systemd/logind.conf
+
---
+++ agnostic/setup-systemd.sh
@@ -0,0 +1,5 @@
+source ../utils.sh
+
+cp /etc/systemd/logind.conf /etc/systemd/logind.conf.lxspold
+lxspr_cp /etc/systemd/logind.conf
+
