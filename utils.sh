6ea64fc6fe5397d4a1a590751704c2b341704c26
RenoirTan <renoirtan2005@gmail.com>
2022-01-14 11:57:27 +0800

    main: add normal user mode functions

--- utils.sh
+++ utils.sh
@@ -1,20 +1,40 @@
 lxspr_path() {
-	echo "fs$1"
+	echo -n "fs$1"
+}
+
+lxsp_path() {
+	echo -n "user/$1"
 }
 
 lxspr_cp() {
 	cp "$(lxspr_path $1)" "$1"
 }
 
+lxsp_cp() {
+	cp "$(lxsp_path $1)" "$HOME/$1"
+}
+
 lxspr_mv() {
 	mv "$(lxspr_path $1)" "$1"
+}
+
+lxsp_mv() {
+	mv "$(lxsp_path $1)" "$HOME/$1"
 }
 
 lxspr_echof() {
 	echo "$(lxspr_path $1)" "$1"
 }
 
+lxsp_echof() {
+	echo "$(lxsp_path $1)" "$HOME/$1"
+}
+
 lxspr_backup() {
+	cp -r "$1" "$1.old"
+}
+
+lxsp_backup() {
 	cp -r "$1" "$1.old"
 }
 
@@ -23,7 +43,16 @@ lxspr_replace() {
 	lxspr_cp "$1"
 }
 
+lxsp_replace() {
+	lxsp_backup "$1"
+	lxsp_cp "$1"
+}
+
 lxspr_restore() {
+	mv "$1.old" "$1"
+}
+
+lxsp_restore() {
 	mv "$1.old" "$1"
 }
 
