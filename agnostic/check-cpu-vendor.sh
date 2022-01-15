2c989fff50f73b04b64b848c47f3f2cee50a5846
RenoirTan <renoirtan2005@gmail.com>
2022-01-15 11:44:04 +0800

    main: my package lists

---
+++ agnostic/check-cpu-vendor.sh
@@ -0,0 +1,1 @@
+lscpu | grep -m 1 "Vendor ID" | awk '{print $3}'
---
+++ distro/arch/install-packages.sh
@@ -0,0 +1,36 @@
+source ../utils.sh
+
+# I could have stored the command in a variable and used exec as yay is
+# CLI-compatible with pacman.
+# However zsh keeps crashing whenever I try to use exec.
+
+command -v pacman > /dev/null && pacman_installed=1
+command -v yay > /dev/null && yay_installed=1
+
+if [ "$yay_installed" != 1 ]; then
+	echo "Warning! Yay has not been installed yet."
+	echo "Run 'setup distro/arch/install-yay.sh' to install yay."
+fi
+
+if [ "$pacman_installed" != 1 && "$yay_installed" != 1 ]; then
+	echo "No supported package managers found."
+	echo "Please install either pacman or yay."
+fi
+
+for collection in $@; do
+	colfile="./$collection-packages.txt"
+	if [ ! -f "$colfile" ]; then
+		echo "Skipping $collection because $colfile does not exist."
+		continue
+	fi
+	if [ "$yay_installed" == 1 ]; then
+		yay -Syuv - < "$colfile"
+	else
+		if [ "$collection" == "yay" ]; then
+			echo "Skipping $collection because yay has not been installed."
+			continue
+		fi
+		sudo pacman -Syuv - < "$colfile"
+	fi
+done
+
