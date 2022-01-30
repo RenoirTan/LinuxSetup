6b714aba6e9b567518492aa0a343b360628d15d8
RenoirTan <renoirtan2005@gmail.com>
2022-01-30 12:01:43 +0800

    main: add tsched=0 part

--- agnostic/setup-pulseaudio-anticrackling.sh
+++ agnostic/setup-pulseaudio-anticrackling.sh
@@ -1,5 +1,6 @@
 source ../utils.sh
 
 lxspr_backup "/etc/pulse/default.pa"
+sed 's/^\s*load-module module-udev-detect\s*$/load-module module-udev-detect tsched=0/g'
 cat "./pulseaudio-microphone-crackling-defaultpa.txt" | tee "/etc/pulse/default.pa"
 
