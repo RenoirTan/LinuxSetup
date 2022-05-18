ec17e7cbc6170d2eb94648a7573205d13ae89f99
RenoirTan <renoirtan2005@gmail.com>
2022-05-18 21:27:46 +0800

    main: update nvidia install process

--- distro/arch/install-nvidia.sh
+++ distro/arch/install-nvidia.sh
@@ -2,7 +2,7 @@ source ../../utils.sh
 
 lxspr_cp /etc/pacman.d/hooks/nvidia.hook
 
-pacman -Syu nvidia
+pacman -Syu < './packages/nvidia-packages.txt'
 
 echo "Restart now for changes to take effect."
 
