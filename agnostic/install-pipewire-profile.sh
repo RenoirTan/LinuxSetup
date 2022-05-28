92283b2d2c6f9d1641a9830d4f61ca27db49a976
RenoirTan <renoirtan2005@gmail.com>
2022-05-28 21:26:58 +0800

    main: pipewire updates

--- agnostic/install-pipewire-profile.sh
+++ agnostic/install-pipewire-profile.sh
@@ -1,3 +1,6 @@
 source ../utils.sh
 
 lxsp_replace .config/easyeffects/input/pipewire.json
+echo "At this point, you should open easyeffects, set 'pipewire' as the preset"
+echo "for input and turn your input microphone volume (not EasyEffects Source)"
+echo "to 50%. From 60% onwards, audio quality will begin to deteriorate."
