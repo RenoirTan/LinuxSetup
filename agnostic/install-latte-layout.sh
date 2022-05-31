53ce420966aff31f71bb009c36873ff8e7cc35b4
RenoirTan <renoirtan2005@gmail.com>
2022-05-31 11:57:18 +0800

    main: wayland version of my latte dock layout

--- agnostic/install-latte-layout.sh
+++ agnostic/install-latte-layout.sh
@@ -1,6 +1,7 @@
 source ../utils.sh
 
 lxsp_replace ".config/latte/Renoir MacOS.layout.latte"
+lxsp_replace ".config/latte/Renoir MacOS (Wayland).layout.latte"
 
 echo "Make sure you install all required widgets before using the layout."
 
