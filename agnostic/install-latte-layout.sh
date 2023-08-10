00f561e4c1aeb6b89e6e9b2a096e66b79cb0faf5
RenoirTan <renoirtan2005@gmail.com>
2023-08-10 22:36:43 +0800

    main: update latte layouts

--- agnostic/install-latte-layout.sh
+++ agnostic/install-latte-layout.sh
@@ -1,6 +1,9 @@
 source ../utils.sh
 
-lxsp_replace ".config/latte/Renoir MacOS.layout.latte"
-lxsp_replace ".config/latte/Renoir MacOS (Wayland).layout.latte"
+lxsp_replace ".config/latte/Renoir MacOS (Wayland, Plasma 5.24).layout.latte"
+lxsp_replace ".config/latte/Renoir MacOS (Wayland, Plasma 5.25).layout.latte"
+lxsp_replace ".config/latte/Renoir MacOS (XOrg, Plasma 5.24).layout.latte"
+lxsp_replace ".config/latte/Renoir MacOS (XOrg, Plasma 5.25).layout.latte"
+lxsp_replace ".config/latte/Renoir MacOS (XOrg, Plasma 5.25, Sidebar).layout.latte"
 
 echo "Read 'agnostic/LATTE DOCK.md' for important information."
